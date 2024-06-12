5dc872bf53984c92862186ad49a3d9d19ffd3305
Kingcedru <byishimocedrick@gmail.com>
2024-06-12 21:25:24 +0200

    added input fields

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -8,49 +8,35 @@ import {
   Pressable,
   TouchableOpacity,
   Switch,
+  Modal,
+  TextInput,
 } from "react-native";
 import React, { useState } from "react";
 
 export default function App() {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [text, setText] = useState("");
   return (
     <View>
       <StatusBar barStyle="default" />
+      <Text>Hello world</Text>
       <Image
         style={{ width: 100, height: 50 }}
         source={require("./assets/icon.png")}
       />
-      <ImageBackground
-        style={{ height: 150 }}
-        source={require("./assets/icon.png")}
-      >
-        <Text>Hello World!</Text>
-      </ImageBackground>
-      <Button
-        title="Click me"
-        color="#000"
-        accessibilityLabel="Click me button"
-        onPress={() => console.log("clicked")}
+      {/* <Button title="Open Modal" onPress={() => setModal(true)} />
+      <Modal visible={modal} animationType="slide">
+        <View>
+          <Text>This is a modal</Text>
+          <Button title="Close Modal" onPress={() => setModal(false)} />
+        </View>
+      </Modal> */}
+      <TextInput
+        placeholder="Write here..."
+        style={{ borderWidth: 2, borderColor: "red", width: "100%" }}
+        value={text}
+        onChangeText={setText}
       />
-      <Pressable
-        style={{ padding: 23, backgroundColor: "blue" }}
-        onPress={() => console.log("press me blue")}
-      >
-        <Text style={{ color: "white" }}>Press me</Text>
-      </Pressable>
-      <TouchableOpacity
-        style={{ padding: 23, backgroundColor: "red" }}
-        onPress={() => console.log("press me red")}
-      >
-        <Text style={{ color: "white" }}>Hello World!</Text>
-      </TouchableOpacity>
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-        value={isEnabled}
-        onValueChange={toggleSwitch}
-      />
+      <Text>{text}</Text>
     </View>
   );
 }
