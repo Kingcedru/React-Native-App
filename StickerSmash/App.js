1fc2078fcd344f998497f89a75679d8b90bbb353
Kingcedru <byishimocedrick@gmail.com>
2024-06-12 18:44:55 +0200

    applied styles

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  StatusBar,
+  Image,
+  ImageBackground,
+  Button,
+  Pressable,
+} from "react-native";
+import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar barStyle="default" />
+      <Image
+        style={{ width: 100, height: 50 }}
+        source={require("./assets/icon.png")}
+      />
+      <ImageBackground
+        style={{ height: 150 }}
+        source={require("./assets/icon.png")}
+      >
+        <Text>Hello World!</Text>
+      </ImageBackground>
+      <Button
+        title="Click me"
+        color="#000"
+        accessibilityLabel="Click me button"
+        onPress={() => console.log("clicked")}
+      />
+      <Pressable
+        style={{ padding: 23, backgroundColor: "blue" }}
+        onPress={() => console.log("press me")}
+      >
+        <Text style={{ color: "white" }}>Press me</Text>
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
